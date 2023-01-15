fe4383108d4a9ebf036e592ea8a2ae9bdee952a8
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2023-01-15 18:24:47 -0300

    Testando o modelo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- <button v-on:click="sendMessage('hello')">Send Message</button> -->
+  <h2>Welcome to Websockets</h2>
+  <p>You are: {{ connectedStatus }}</p>
+  <p>Your message is: {{ message }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  data() {
+    return {
+        socket: {},
+        connectedStatus: 'Not connected!',
+        message: 'No message yet!'
+    }
+  },
+  async mounted() {
+      // Calculate the URL for the websocket. If you have a fixed URL, then you can remove all this and simply put in
+      // ws://your-url-here.com or wss:// for secure websockets.
+      const socketProtocol = (window.location.protocol === 'https:' ? 'wss:' : 'ws:')
+      console.log(socketProtocol);
+      const port = ':8080';
+      const echoSocketUrl = socketProtocol + '//' + window.location.hostname + port + '/ws'
+  
+      // Define socket and attach it to our data object
+      console.log(echoSocketUrl);
+      console.log(this.connectedStatus);
+      this.socket = await new WebSocket(echoSocketUrl); 
+      console.log(this.socket);
+
+      // When it opens, console log that it has opened. and send a message to the server to let it know we exist
+      this.socket.onopen = () => {
+          console.log('Websocket connected.');
+          this.connectedStatus = 'Connected';
+          this.sendMessage(JSON.stringify({"message" : "Hello, server."}));
+      }
+
+      // When we receive a message from the server, we can capture it here in the onmessage event.
+      this.socket.onmessage = (event) => {
+          // We can parse the data we know to be JSON, and then check it for data attributes
+          let parsedMessage = JSON.parse(event.data);
+          // If those data attributes exist, we can then console log or show data to the user on their web page.
+          console.log(parsedMessage);
+          if(typeof parsedMessage.message !== "undefined" && parsedMessage.message == "Conectou!") {
+              this.message = parsedMessage.message;
+              console.log('We have received a message from the server!')
+          }
+      }
+  },
+  methods: {
+    waitForOpenConnection: function() {
+        // We use this to measure how many times we have tried to connect to the websocket server
+        // If it fails, it throws an error.
+        return new Promise((resolve, reject) => {
+            const maxNumberOfAttempts = 10
+            const intervalTime = 200 
+
+            let currentAttempt = 0
+            const interval = setInterval(() => {
+                if (currentAttempt > maxNumberOfAttempts - 1) {
+                    clearInterval(interval)
+                    reject(new Error('Maximum number of attempts exceeded.'));
+                } else if (this.socket.readyState === this.socket.OPEN) {
+                    clearInterval(interval)
+                    resolve()
+                }
+                currentAttempt++
+            }, intervalTime)
+        })
+    },
+    sendMessage: async function(message) {
+        // We use a custom send message function, so that we can maintain reliable connection with the
+        // websocket server.
+        console.log('sendMessage: ', message)
+        if (this.socket.readyState !== this.socket.OPEN) {
+            try {
+                await this.waitForOpenConnection(this.socket)
+                this.socket.send(message)
+            } catch (err) { console.error(err) }
+        } else {
+            console.log('Entrei no else')
+            this.socket.send(message)
+        }
+    }
   }
 }
 </script>
